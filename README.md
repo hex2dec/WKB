ad439cbba9fb4c56f929b6672c83418ee4efc60c
William Huang <ai.william@outlook.com>
2020-07-18 21:53:48 +0800

    setup nextjs with typescript
    Signed-off-by: William Huang <ai.william@outlook.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# William's Knowledge Base
+
+## Development
+
+```shell
+npm install
+npm run dev
+```
+
+## Copyright
+
+The WKB Authors
+
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home | WKB">
+    <h1>Hello WKB 👋</h1>
+  </Layout>
+)
+
+export default IndexPage
